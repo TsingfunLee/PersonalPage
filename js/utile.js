298c426397fc13ba0636ff06d9a767f14214f5d2
李庆芳 <867259206@qq.com>
2016-10-13 20:30:36 +0800

    compatible

--- js/utile.js
+++ js/utile.js
@@ -86,3 +86,24 @@ window.utils.captureTouch = function(element) {
 	//返回touch对象
 	return touch;
 };
+
+//动画循环
+if(!window.requestAnimationFrame) {
+	window.requestAnimationFrame = (window.webkitRequestAnimationFrame ||
+		window.mozRequestAnimationFrame ||
+		window.msRequestAnimationFrame ||
+		window.oRequestAnimationFrame ||
+		function(callback) {
+			return window.setTimeout(callback, 17 /*~ 1000/60*/ );
+		});
+}
+
+//动画循环取消
+if(!window.cancelAnimationFrame) {
+	window.cancelAnimationFrame = (window.cancelRequestAnimationFrame ||
+		window.webkitCancelAnimationFrame || window.webkitCancelRequestAnimationFrame ||
+		window.mozCancelAnimationFrame || window.mozCancelRequestAnimationFrame ||
+		window.msCancelAnimationFrame || window.msCancelRequestAnimationFrame ||
+		window.oCancelAnimationFrame || window.oCancelRequestAnimationFrame ||
+		window.clearTimeout);
+}
