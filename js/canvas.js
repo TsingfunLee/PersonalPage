41c0914e52b3beab4ae4fa850075b28e26a70dcf
李庆芳 <867259206@qq.com>
2017-01-17 19:33:05 +0800

    change animation effect

--- js/canvas.js
+++ js/canvas.js
@@ -140,7 +140,7 @@
 
     function shiftPoint(p) {
         TweenLite.to(p, 1+1*Math.random(), {x:p.originX-50+Math.random()*100,
-            y: p.originY-50+Math.random()*100, ease:Power4.easeInOut,
+            y: p.originY-50+Math.random()*100, ease:Circle.easeInOut,
             onComplete: function() {
                 shiftPoint(p);
             }});
